a0ba06b510ad55a6ab273cebe50b860b5bc2fde1
Ethan Henderson <ethan@zbee.codes>
2024-08-06 01:32:38 -0600

    Clean up comment text

--- worker.js
+++ worker.js
@@ -429,12 +429,11 @@ async function post_comment_on_repo(trigger_data, env) {
                 + "- **Target Name**: `" + trigger_data.target_name + "`\n"
                 + "- **Target Repository**: `" + trigger_data.target_repo + "`\n"
                 + "- **Main Branch**: `" + trigger_data.branch_main + "`"
-                + " (build with `" + trigger_data.branch_main_build + "`)"
+                + " (with `" + trigger_data.branch_main_build + "` config)"
                 + "\n"
                 + (trigger_data.branch_test !== null
                     ? "- **Test Branch**: `" + trigger_data.branch_test + "`"
-                    + " (build with `" + trigger_data.branch_main_build + "`)"
-                    + "\n"
+                    + " (with `" + trigger_data.branch_main_build + "` config)"+ "\n"
                     : "")
                 + "\n\n\n"
                 + "<details><summary>Raw Trigger Data</summary>"
